51c86ef1981a460ddada6d8fed7d499eae9a81e6
Kakakakmama12 <nishanroka890@gmail.com>
2024-03-06 13:24:38 +0545

    PersonalDetails.java

---
+++ Week1/PersonalDetails.java
@@ -0,0 +1,18 @@
+package week1;
+
+public class PersonalDetails {
+    public static void main(String[] args) {
+        // Personal details
+        String name = "Nishan Roka";
+        String address = "Lalitpur";
+        int age = 20;
+        String phoneNumber = "98223344556";
+
+        // Output personal details
+        System.out.println("Name: " + name);
+        System.out.println("Address: " + address);
+        System.out.println("Age: " + age);
+        System.out.println("Phone Number: " + phoneNumber);
+    }
+}
+
